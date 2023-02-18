06dd6b9e3abf8279ddda5997f07d2a80d5e7812c
JCamyre <jwcamry03@gmail.com>
2023-02-18 14:20:34 -0800

    Add animations

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 
+// Maybe add hover stuff
 const StyledCard = styled.div`
   width: 252px;
   height: 435px;
   background-color: transparent;
+  padding: 10px;
+  transition: ease-out 0.1s;
+  &:hover {
+    cursor: pointer;
+    transform: scale(0.95);
+  }
 `;
 const CardImage = styled.img`
   max-width: 100%;
@@ -14,7 +21,7 @@ const CardImage = styled.img`
 
 function Card() {
   return (
-    <StyledCard>
+    <StyledCard onClick={() => console.log("yo")}>
       <CardImage
         src="https://i.pinimg.com/750x/f4/d1/9c/f4d19cecd5d24e19376ac78d30131c97.jpg"
         alt="Car"
